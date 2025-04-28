da234f4e4bcb06541da14a967c17b70b7af72e7c
ewrika <egorkaomsk_2003@mail.ru>
2025-04-28 22:56:44 +0600

    Initial Commit

---
+++ Kursach/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kursach
+//
+//  Created by Георгий Борисов on 28.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kursach/KursachApp.swift
@@ -0,0 +1,17 @@
+//
+//  KursachApp.swift
+//  Kursach
+//
+//  Created by Георгий Борисов on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct KursachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
